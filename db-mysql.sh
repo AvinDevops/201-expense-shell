63414d22f328a158723da58552f52faffd380cc1
AvinPracticeDevOps <avinashkbandela@gmail.com>
2025-07-26 18:52:04 +0530

    installing mysql server

--- db-mysql.sh
+++ db-mysql.sh
@@ -41,5 +41,12 @@ VALIDATE $? "Enabiling mysqld"
 systemctl start mysqld &>>$LOGFILE
 VALIDATE $? "Starting mysqld"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>LOGFILE
-VALIDATE $? "Setting password for root user"
+mysql -h 172.31.22.129 -uroot -pExpenseApp@1 -e 'show databases;'
+if [ $? -ne 0 ]
+then
+    mysql_secure_installation --set-root-pass ExpenseApp@1 &>>LOGFILE
+    VALIDATE $? "Setting password for root user"
+else
+    echo -e "Root password is already set....$Y SKIPPING $N"
+fi
+
